eb81d6b5c679f03f705bd3dbe47d75f6a0ea8d4f
Northun <37567807+Northun@users.noreply.github.com>
2025-08-24 23:11:02 -0400

    Update practice_python.py

--- practice/practice_python.py
+++ practice/practice_python.py
@@ -144,3 +144,38 @@ def maxArea(self, height: List[int]) -> int:
             right = right - 1
     
     return container
+
+#Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+#Notice that the solution set must not contain duplicate triplets.
+
+def threeSum(self, nums: List[int]) -> List[List[int]]:
+    lenS = len(nums)
+    triplets = []
+    nums.sort()
+
+    for i in range(lenS):
+        if i > 0 and nums[i] == nums[i-1]:
+            continue;
+
+        left = i+1
+        right = lenS-1
+        while left < right:
+            sums = nums[i] + nums[left] + nums[right]
+
+            if sums == 0:
+                triplets.append([nums[i], nums[left], nums[right]])
+
+                while left < right and nums[left] == nums[left+1]:
+                    left+= 1
+                while left < right and nums[right] == nums[right-1]:
+                    right-= 1
+                left+= 1
+                right-= 1
+            
+            elif sums < 0:
+                left+= 1
+            else:
+                right-= 1
+    
+    return triplets
