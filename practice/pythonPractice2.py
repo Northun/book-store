41b67a3c42efea4db41ab3641841cd8c9cc82159
Northun <37567807+Northun@users.noreply.github.com>
2025-08-28 09:54:54 -0400

    Update pythonPractice2.py

--- practice/pythonPractice2.py
+++ practice/pythonPractice2.py
@@ -28,3 +28,35 @@ def convert(self, s: str, numRows: int) -> str:
         output[i] = ''.join(output[i])
 
     return ''.join(output)
+
+#Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+#Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+def reverse(self, x: int) -> int:
+    sign = True if x > 0 else False
+    INT_MAX = 2**31 - 1  # 2147483647
+    INT_MIN = -2**31  
+    
+    first_digit_pos = int(str(abs(INT_MAX))[-1])
+    other_digit_pos = int(str(abs(INT_MAX))[:-1])
+    
+    first_digit_neg = int(str(abs(INT_MIN))[-1])
+    other_digit_neg = int(str(abs(INT_MIN))[:-1])
+
+    #devide by 10,100,... get the remainder as the reverse
+    result  = 0
+    while x != 0:
+        
+        remainder = x%10 if x >0 else -(abs(x)%10)
+        # check if overflow
+        if sign and (result > other_digit_pos or (result == other_digit_pos and remainder > first_digit_pos)):
+            return 0
+        elif not sign and (result < -other_digit_neg or (result == -other_digit_neg and remainder < -first_digit_neg)):
+            return 0
+        
+        result = result*10 + remainder
+        
+        x = x // 10 if x >0 else -(abs(x)//10)
+
+    return result
