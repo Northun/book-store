33e6911631d024cc9d473adf7c0bbc0ea7770081
Northun <37567807+Northun@users.noreply.github.com>
2025-09-01 23:49:52 -0400

    Update pythonPractice2.py

--- practice/pythonPractice2.py
+++ practice/pythonPractice2.py
@@ -160,3 +160,36 @@ def isPalindrome(self, x: int) -> bool:
     #reverse = reverse*10 + xCopy//10
 
     return reverse == x
+
+#Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+def letterCombinations(self, digits: str) -> List[str]:
+    if not digits:
+        return []
+
+    digitMap = {
+        '2':'abc',
+        '3':'def',
+        '4': 'ghi',
+        '5': 'jkl',
+        '6': 'mno',
+        '7': 'pqrs',
+        '8': 'tuv',
+        '9': 'wxyz',
+    }
+
+    idxMax = len(digits)
+
+    def combine(idx, combination):
+        if idx == idxMax:
+            result.append(combination[:])
+            # we don't return value since we modify result already
+            return
+        
+        
+        for ch in digitMap[digits[idx]]:
+            combine(idx+1, combination + ch)
+
+    result = []
+    combine(0,"")
+
+    return result
