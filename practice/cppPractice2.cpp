19d56da9cf8de02e3ddb2510eb5ed8dccf72314d
Northun <37567807+Northun@users.noreply.github.com>
2025-09-08 00:52:49 -0400

    Update cppPractice2.cpp

--- practice/cppPractice2.cpp
+++ practice/cppPractice2.cpp
@@ -315,3 +315,33 @@ void perm(vector<int>& nums, int start, vector<vector<int>>& result) {
         swap(nums[start], nums[i]);      
     }
 }
+
+# Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        lenS = len(nums)
+        right = lenS - 1
+
+        leftnum = nums[0]
+        rightnum = nums[-1]
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+
+            if nums[left] <= nums[mid]:
+                #left side is sorted
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+
+            else:
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        
+        return -1
