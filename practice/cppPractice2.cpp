65cbb3543f12b10f4fcaf580bb9f695b43d43194
Northun <37567807+Northun@users.noreply.github.com>
2025-09-12 00:01:51 -0400

    Update cppPractice2.cpp

--- practice/cppPractice2.cpp
+++ practice/cppPractice2.cpp
@@ -374,3 +374,36 @@ void nextPermutation(vector<int>& nums) {
 
     reverse(nums.begin()+canUp, nums.end());
 }
+
+//  Integer to Roman
+
+string intToRoman(int num) {
+    //define an array of tuples from value to symbol
+    std:array<pair<int, string>, 13> ValToSym = {
+        {
+        {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"}, {100, "C"},
+        {90, "XC"},  {50, "L"},   {40, "XL"}, {10, "X"},   {9, "IX"},
+        {5, "V"},    {4, "IV"},   {1, "I"}
+        }
+    };
+
+    string result;
+
+    for (const auto& [value, symbol] : ValToSym){
+        if (num == 0)
+            break;
+
+        while (num >= value) {
+            result += symbol;
+            num -= value;
+        }
+        
+        //int repeat = num/value;
+        // won't work when symbol is not char
+        //string s(repeat, symbol);
+        //result += s;
+        //num = num - repeat*value;
+    }
+    
+    return result;
+}
