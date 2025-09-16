d3585a08aeb5dcc97ed48282ffa1cb9487d4c15b
Northun <37567807+Northun@users.noreply.github.com>
2025-09-15 23:18:11 -0400

    Update pythonPractice2.py

--- practice/pythonPractice2.py
+++ practice/pythonPractice2.py
@@ -351,3 +351,23 @@ def countAndSay(self, n: int) -> str:
         return "1"
     else:
         return rle(self.countAndSay(n-1))
+
+#Input: nums = [2,3,1,1,4]
+#Output: 2
+#Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
+    
+def jump(self, nums: List[int]) -> int:
+
+    near = far = 0
+    jumps = 0
+
+    while far < len(nums) - 1:
+        farthest = 0
+        for i in range(near, far + 1):
+            farthest = max(farthest, i + nums[i])
+        
+        near = far + 1
+        far = farthest
+        jumps += 1
+    
+    return jumps
